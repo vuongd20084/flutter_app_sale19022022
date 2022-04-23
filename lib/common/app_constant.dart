1cf463c95d339de6d4b51ff3ebb97ea484c896c1
lehongquocvuong <vuongd20084@cusc.ctu.edu.vn>
2022-04-23 10:30:26 +0700

    Setup

---
+++ lib/common/app_constant.dart
@@ -0,0 +1,10 @@
+class AppConstant{
+  // Name route
+  static const String LOGIN_ROUTE_NAME = "/login";
+
+  //Image path
+  static const String BANNER_1 = "assets/images/banner_1.jpg";
+
+  //host name
+  static const String BASE_URL = "https://appsale.herokuapp.com/";
+}
